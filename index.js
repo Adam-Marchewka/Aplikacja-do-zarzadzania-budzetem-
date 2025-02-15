dfc04d3a1c3ae8af2531abf210f8b3f17c351b52
Adam Marchewka <adam.marchewka216@gmail.com>
2025-02-15 17:23:50 +0100

    Dodanie formularza do index.html

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const expensesChart = document.getElementById('expenses-chart');
 const incomeChart = document.getElementById('income-chart');
 
-new Chart(expensesChart, {
+new Chart(incomeChart, {
     type: 'doughnut',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
-        label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            '#4CAF50', 
+            '#8BC34A', 
+            '#2196F3', 
+            '#03A9F4', 
+            '#FFC107', 
+            '#FF9800'
+        ],
+        hoverBackgroundColor: [
+            '#43A047', 
+            '#7CB342', 
+            '#1E88E5', 
+            '#039BE5', 
+            '#FFB300', 
+            '#FB8C00'
+        ],
         borderWidth: 1
       }]
     },
@@ -20,13 +35,30 @@ new Chart(expensesChart, {
     }
 });
 
-new Chart(incomeChart, {
+new Chart(expensesChart, {
     type: 'doughnut',
     data: {
       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
       datasets: [{
-        label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            '#F44336',
+            '#B71C1C',
+            '#FF9800',
+            '#FF5722',
+            '#795548',
+            '#5D4037',
+            '#9E9E9E'
+          ],
+          hoverBackgroundColor: [
+            '#E53935',
+            '#D32F2F',
+            '#FB8C00',
+            '#F4511E',
+            '#6D4C41',
+            '#4E342E',
+            '#757575'
+          ],
         borderWidth: 1
       }]
     },
