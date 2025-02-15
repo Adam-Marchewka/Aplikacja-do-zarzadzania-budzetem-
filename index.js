764d83569129123e5c5702ac0ae81035ae1dcdb9
Adam Marchewka <adam.marchewka216@gmail.com>
2025-02-15 15:17:53 +0100

    Dodanie wykresów do index.html

---
+++ index.js
@@ -0,0 +1,40 @@
+const expensesChart = document.getElementById('expenses-chart');
+const incomeChart = document.getElementById('income-chart');
+
+new Chart(expensesChart, {
+    type: 'doughnut',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+});
+
+new Chart(incomeChart, {
+    type: 'doughnut',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+});
